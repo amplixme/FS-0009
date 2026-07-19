f818fec3233b4e4b49c5abff403cda7bbecabf6b
Fabian Larrosa <fabianlarrosa2013@gmail.com>
2026-07-19 11:57:17 -0300

    fix: se corrige condiciones para que controle correctamente

--- BackEnd/src/middlewares/role.middleware.js
+++ BackEnd/src/middlewares/role.middleware.js
@@ -1,9 +1,20 @@
 
 const requireRole = (...roles) => {
     return (req, res, next) => {
+
+        if (!req.user || !req.user.role) {
+            return res.status(403).json({
+                error: {
+                    message: "No tienes permisos para realizar esta acción"
+                }
+            });
+        }
+
         const rolUpper = req.user.role.toUpperCase();
 
-        if (!req.user || roles.includes(rolUpper)) {
+        const allowedRolesUpper = roles.map(role => role.toUpperCase());
+
+        if (!allowedRolesUpper.includes(rolUpper)) {
             return res.status(403).json({
                 error: {
                     message: "No tienes permisos para esta acción"
