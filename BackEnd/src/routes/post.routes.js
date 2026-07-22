69ea4f5b149f39b7c70a71fa34fa9ea9d6fc343a
Tierrez <tdetierrez@gmail.com>
2026-07-22 15:09:39 -0300

    feat: agregar endpoints para listar posts y obtener post por ID

--- BackEnd/src/routes/post.routes.js
+++ BackEnd/src/routes/post.routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
-import { create } from '../controllers/post.controller.js';
+import { create, getAll, getById} from '../controllers/post.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import { validate } from '../middlewares/validate.middleware.js'; 
 import { createPostSchema } from '../schema/post.schema.js';
 
 const router = Router();
 
-// Endpoints
+// rutas publicas
+router.get('/', getAll);
+router.get('/:id', getById);
+
+// ruta protegida
 router.post('/', authMiddleware, validate(createPostSchema), create);
+
+
 
 export default router;
--- BackEnd/src/services/post.service.js
+++ BackEnd/src/services/post.service.js
@@ -26,5 +26,52 @@ export const createPostService = async ({ title, content, authorId }) => {
     },
   });
 
+  
+
   return newPost;
+};
+
+export const getAllPostsService = async () => {
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      published: true,
+      createdAt: true,
+      updatedAt: true,
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return posts;
+};
+
+// Obtener un post por su ID
+export const getPostByIdService = async (id) => {
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      published: true,
+      createdAt: true,
+      updatedAt: true,
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return post;
 };
